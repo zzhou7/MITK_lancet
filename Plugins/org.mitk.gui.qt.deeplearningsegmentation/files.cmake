b704450312fc449142c8c22aed23b70e09aaf945
Kim-Celine Kahl <kim-celine.kahl@web.de>
2020-11-05 09:54:37 +0100

    deleted plugin view of deeplearning segmentation

--- Plugins/org.mitk.gui.qt.deeplearningsegmentation/files.cmake
+++ Plugins/org.mitk.gui.qt.deeplearningsegmentation/files.cmake
@@ -21,8 +21,6 @@ set(MOC_H_FILES
 # for example the icon used in the menu and tabs for the
 # plug-in views in the workbench
 set(CACHED_RESOURCE_FILES
-  resources/icon.xpm
-  plugin.xml
 )
 
 # list of Qt .qrc files which contain additional resources
