a17a2f133810057ad21a8760e31c5b449324781b
Kim-Celine Kahl <k.kahl@dkfz-heidelberg.de>
2021-02-16 10:54:14 +0100

    get python test running on Ubuntu

--- Modules/PythonService/mitkPythonService.cpp
+++ Modules/PythonService/mitkPythonService.cpp
@@ -38,6 +38,7 @@ found in the LICENSE file.
 #endif
 
 #include "swigpyrun.h"
+#include <boost/algorithm/string.hpp>
 
 typedef itksys::SystemTools ist;
 
@@ -45,6 +46,14 @@ mitk::PythonService::PythonService()
   : m_ItkWrappingAvailable(true),
     m_ErrorOccured(false)
 {
+  // for Linux, libpython needs to be opened before initializing the Python Interpreter to enable imports
+  #ifndef WIN32
+    std::vector<std::string> pythonIncludeVector;
+    // get libpython file to open (taken from "Python3_INCLUDE_DIR" variable from CMake to dynamically do it for different python versions)
+    boost::split(pythonIncludeVector, PYTHON_INCLUDE, boost::is_any_of( "/" ) );
+    std::string libPython = "lib"+pythonIncludeVector[pythonIncludeVector.size()-1]+".so";
+    dlopen(libPython.c_str(), RTLD_LAZY | RTLD_GLOBAL);
+  #endif
   // Initialize Python interpreter and ensure global interpreter lock in order to execute python code safely
   if (!Py_IsInitialized())
   {
