34d637a6885a4e4b14c37437098699b4f87f0448
zzhou7 <zzhou.be07@nycu.edu.tw>
2022-05-26 11:03:43 +0800

    IGT module update
    from Zhao

--- Modules/IGT/files.cmake
+++ Modules/IGT/files.cmake
@@ -17,6 +17,7 @@ set(CPP_FILES
   Algorithms/mitkIGTLMessageToNavigationDataFilter.cpp
   Algorithms/mitkNeedleProjectionFilter.cpp
   Algorithms/mitkPivotCalibration.cpp
+  Algorithms/mitkNavigationDataInReferenceCoordFilter.cpp
 
   Common/mitkIGTTimeStamp.cpp
   Common/mitkSerialCommunication.cpp
